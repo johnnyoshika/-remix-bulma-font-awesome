348adbc328d60ddedea1aca5dd0b2f659cf4ca40
Johnny Oshika <johnnyoshika@gmail.com>
2022-01-14 22:48:06 -0800

    Install fontawesome-free

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,4 +1,6 @@
-import { hydrate } from "react-dom";
-import { RemixBrowser } from "remix";
+import { hydrate } from 'react-dom';
+import { RemixBrowser } from 'remix';
+
+import '@fortawesome/fontawesome-free/js/all.js';
 
 hydrate(<RemixBrowser />, document);
